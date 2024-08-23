f90e366156d1942718c82a2aef6755b9a3cb07de
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-08-23 06:13:27 -0400

    Creando el modelo de Producto y Agregados

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.CharField(max_length=100)
+    description=models.CharField(max_length=100)
+    price=models.DecimalField()
+
+class Aggregate(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField()
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
