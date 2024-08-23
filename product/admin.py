0db72bfab3c516f19dbe76b6a3862dbaff67940b
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-08-23 06:42:16 -0400

    Modifica la imagene del producto en el modelo

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Aggregate
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Aggregate)
--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name=models.CharField(max_length=100)
-    image=models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
     description=models.CharField(max_length=100)
     price=models.DecimalField(max_digits=10, decimal_places=2)
 
