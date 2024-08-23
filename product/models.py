eeddb77d7cc411849507224db541ad2ade4be8d1
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-08-23 06:23:50 -0400

    Fixing price in model product and aggregate

--- product/models.py
+++ product/models.py
@@ -5,9 +5,15 @@ class Product(models.Model):
     name=models.CharField(max_length=100)
     image=models.CharField(max_length=100)
     description=models.CharField(max_length=100)
-    price=models.DecimalField()
+    price=models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
+    
 class Aggregate(models.Model):
     name=models.CharField(max_length=100)
-    price=models.DecimalField()
+    price=models.DecimalField(max_digits=10, decimal_places=2)
     product=models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
