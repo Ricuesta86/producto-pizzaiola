4fb6570552accf99f27d9b42042d0c0ddd78cf22
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-08-25 08:16:58 -0400

    Organizando las plantillas html

--- product/views.py
+++ product/views.py
@@ -44,7 +44,7 @@ def signout(request):
 
 def products(request):
     productos=Product.objects.all()
-    return render(request, "products.html", {"productos":productos})
+    return render(request, "products/products.html", {"productos":productos})
 
 def new_product(request):
     if request.method == 'POST':
@@ -55,7 +55,7 @@ def new_product(request):
     else:
         form = ProductForm()
     
-    return render(request, 'new_product.html', {'form': form})
+    return render(request, 'products/new_product.html', {'form': form})
 
 def update_product(request,id):
     producto = get_object_or_404(Product, id=id)  # Obtener el producto por su ID
@@ -69,7 +69,7 @@ def update_product(request,id):
     else:
         form = ProductForm(instance=producto)  # Cargar el formulario con los datos existentes del producto
     
-    return render(request, 'update_product.html', {'form': form, 'producto': producto})
+    return render(request, 'products/update_product.html', {'form': form, 'producto': producto})
 
 
 def detail_product(request, id):
@@ -77,7 +77,7 @@ def detail_product(request, id):
     producto = get_object_or_404(Product, id=id)
     
     # Renderizar la plantilla con el producto encontrado
-    return render(request, 'detail_product.html', {'producto': producto})
+    return render(request, 'products/detail_product.html', {'producto': producto})
 
 def delete_product(request, id):
     # Obtener el producto con el ID proporcionado, o lanzar un 404 si no se encuentra
@@ -87,4 +87,4 @@ def delete_product(request, id):
         producto.delete()  # Eliminar el producto
         return redirect('products')  # Redirigir a la lista de productos
     
-    return render(request, 'delete_product.html', {'producto': producto})
+    return render(request, 'products/delete_product.html', {'producto': producto})
