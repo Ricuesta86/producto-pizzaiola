0b3a95d98fbc281436b75add9d8df627b7e18a93
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-08-30 16:05:19 -0400

    Add estilos a crear producto

--- product/forms.py
+++ product/forms.py
@@ -6,6 +6,12 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["name", "description", "price", "imagen"]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter el nombre del producto'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Enter la descripción del producto'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter el precio del producto'}),
+            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
 
 
 class AggregateForm(forms.ModelForm):
