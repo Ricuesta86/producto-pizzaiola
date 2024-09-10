1cae0df0702e9a36e69bf22240cf9c1abdfcfba1
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-09-09 20:20:19 -0400

    Actualizando pip

--- productdjango/settings.py
+++ productdjango/settings.py
@@ -23,12 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=kr)$d-*(0w3t1$#($zy%985lw=b1g%h6zir-_9dh3o*npz!wo'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-=kr)$d-*(0w3t1$#($zy%985lw=b1g%h6zir-_9dh3o*npz!wo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
