059b2fff6b27565ad0d22deeb6479fc70e8bcce2
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-09-12 07:30:10 -0400

    Config la base de datos en los entornos de desarrollo y produccion

--- productdjango/settings.py
+++ productdjango/settings.py
@@ -81,24 +81,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'productdjango.wsgi.application'
 
+if not DEBUG:
+    # Replace the SQLite DATABASES configuration with PostgreSQL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            # Replace this value with your local database's connection string.
+            default='postgresql://postgres:postgres@localhost:5432/mysite',
+            conn_max_age=600
+        )
+    }
+else:
+    # Database
+    # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# Replace the SQLite DATABASES configuration with PostgreSQL:
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
